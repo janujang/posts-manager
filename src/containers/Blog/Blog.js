6771f8aae96f1df23c1a4bbed50c39b125737f63
Janujan Gathieswaran <janujan.g@hotmail.com>
2019-05-08 17:17:58 -0400

    Changes

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -4,15 +4,32 @@ import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import axios from 'axios';
 
 class Blog extends Component {
+    state = {
+        posts: []
+    }
+    componentDidMount(){
+        //can't do const posts = axios.get() because axios.get() is asynchronous and JS runs code synchronously.
+        //This means that JS won't wait for the data to be fetched
+
+        //Function in then is executed once the promise is resolved
+        //Promise is returned by .get()
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(response => {
+            this.setState({posts: response.data});
+            console.log(response);
+        });
+    }
     render () {
+        const posts = this.state.posts.map(post =>
+            <Post title={post.title} key={post.id}/>
+        );
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
                     <FullPost />
