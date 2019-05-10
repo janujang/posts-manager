3e4fa253916e8f27f1f391ec3ae5d074097b0e82
Janujan Gathieswaran <janujan.g@hotmail.com>
2019-05-10 16:56:14 -0400

    Changes

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -17,7 +17,7 @@ class Blog extends Component {
 
         //Function in then is executed once the promise is resolved
         //Promise is returned by .get()
-        axios.get('https://jsonplaceholder.typicode.com/posts')
+        axios.get('/posts')
         .then(response => {
             const posts = response.data.slice(0,4);
             const updatedPosts = posts.map(post =>{
