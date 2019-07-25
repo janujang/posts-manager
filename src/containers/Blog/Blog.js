5550362f8b5584fb51e50f2b5b24631db2acf5a8
Janujan Gathieswaran <Janujan.g@hotmail.com>
2019-07-25 15:25:59 -0400

    changes

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,51 +1,72 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import axios from 'axios';
-import { Route, NavLink, Switch } from 'react-router-dom';
+import { Route, NavLink, Switch, Redirect } from "react-router-dom";
 
-import './Blog.css';
-import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
-import FullPost from './FullPost/FullPost';
-
+import "./Blog.css";
+import Posts from "./Posts/Posts";
+//import NewPost from "./NewPost/NewPost";
+//import FullPost from "./FullPost/FullPost";
+import asyncComponent from "../../hoc/asyncComponent";
+const AsyncNewPost = asyncComponent(() => {
+  return import("./NewPost/NewPost");
+});
+//const NewPost = React.lazy(() => import("./NewPost/NewPost"));
 class Blog extends Component {
-    render () {
-        return (
-            <div className="Blog">
-                <header>
-                    <nav>
-                        <ul>
-                            {/*exact means full path is '/' */}
-                            <li><NavLink 
-                                    to="/" 
-                                    exact
-                                    activeClassName="my-active"
-                                    activeStyle={{
-                                        color: '#fa923f'
-                                    }}>Home</NavLink></li>
-                            <li><NavLink 
-                                to={{
-                                //relative path when /new-post added to current url
-                                //below is an absolute path 
-                                pathname: '/new-post',
-                                hash: '#submit',
-                                search: '?quick-submit=true'
-                                }}
-                                activeStyle={{
-                                    color: '#fa923f'
-                                }}>New Post</NavLink></li>
-                        </ul>
-                    </nav>
-                </header>
-                {/* <Route path="/" exact render={() => <h1>Home</h1>} />
+  state = { isAuth: false };
+  render() {
+    return (
+      <div className="Blog">
+        <header>
+          <nav>
+            <ul>
+              {/*exact means full path is '/' */}
+              <li>
+                <NavLink
+                  to="/posts"
+                  exact
+                  activeClassName="my-active"
+                  activeStyle={{
+                    color: "#fa923f"
+                  }}
+                >
+                  Posts
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to={{
+                    //relative path when /new-post added to current url
+                    //this.props.match.url + '/new-post'
+
+                    //below is an absolute path
+                    pathname: "/new-post",
+                    hash: "#submit",
+                    search: "?quick-submit=true"
+                  }}
+                  activeStyle={{
+                    color: "#fa923f"
+                  }}
+                >
+                  New Post
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        {/* <Route path="/" exact render={() => <h1>Home</h1>} />
                 <Route path="/" render={() => <h1>Home 2</h1>} /> */}
-                <Switch>
-                    <Route path="/" exact component={Posts}/>
-                    <Route path="/new-post" component={NewPost}/>
-                    <Route path="/:id" exact component={FullPost} />
-                </Switch>
-            </div>
-        );
-    }
+        <Switch>
+          {this.state.isAuth ? (
+            <Route path="/new-post" component={AsyncNewPost} />
+          ) : null}
+          
+          <Route path="/posts" component={Posts} />
+          <Route render={() => <h1>Page not found</h1>} />
+          {/* <Redirect from="/" to="/posts" /> */}
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default Blog;
--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,61 +1,73 @@
-import React, { Component } from 'react';
-import axios from '../../../axios';
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import axios from "../../../axios";
 
-import Post from '../../../components/Post/Post';
-import './Posts.css';
+import Post from "../../../components/Post/Post";
+import FullPost from "../FullPost/FullPost";
+import "./Posts.css";
 
 class Posts extends Component {
-    state = {
-        posts: []
-    }
+  state = {
+    posts: []
+  };
 
-    componentDidMount () {
-        //can't do const posts = axios.get() because axios.get() is asynchronous and JS runs code synchronously.
-        //This means that JS won't wait for the data to be fetched
+  componentDidMount() {
+    //can't do const posts = axios.get() because axios.get() is asynchronous and JS runs code synchronously.
+    //This means that JS won't wait for the data to be fetched
 
-        //Function in then is executed once the promise is resolved
-        //Promise is returned by .get()
-        //console.log(this.props);
-        axios.get( '/posts' )
-            .then( response => {
-                const posts = response.data.slice(0, 4);
-                const updatedPosts = posts.map(post => {
-                    return {
-                        ...post,
-                        author: 'Max'
-                    }
-                });
-                this.setState({posts: updatedPosts});
-                // console.log( response );
-            } )
-            .catch(error => {
-                console.log(error);
-                // this.setState({error: true});
-            });
-    }
+    //Function in then is executed once the promise is resolved
+    //Promise is returned by .get()
+    //console.log(this.props);
+    axios
+      .get("/posts")
+      .then(response => {
+        const posts = response.data.slice(0, 4);
+        const updatedPosts = posts.map(post => {
+          return {
+            ...post,
+            author: "Max"
+          };
+        });
+        this.setState({ posts: updatedPosts });
+        // console.log( response );
+      })
+      .catch(error => {
+        console.log(error);
+        // this.setState({error: true});
+      });
+  }
 
-    postSelectedHandler = (id) => {
-        this.props.history.push({pathname: '/' + id});
-    }
-    
-    render () {
-        let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>;
-        if (!this.state.error) {
-            posts = this.state.posts.map(post => {
-                return <Post 
-                    key={post.id} 
-                    title={post.title} 
-                    author={post.author}
-                    clicked={() => this.postSelectedHandler(post.id)} />;
-            });
-        }
+  postSelectedHandler = id => {
+    this.props.history.push({ pathname: "/posts/" + id });
+  };
 
+  render() {
+    let posts = <p style={{ textAlign: "center" }}>Something went wrong!</p>;
+    if (!this.state.error) {
+      posts = this.state.posts.map(post => {
         return (
-            <section className="Posts">
-                {posts}
-            </section>
+          <Post
+            key={post.id}
+            title={post.title}
+            //{...this.props}
+            author={post.author}
+            clicked={() => this.postSelectedHandler(post.id)}
+          />
         );
+      });
     }
+
+    return (
+      <div>
+        <section className="Posts">{posts}</section>
+        <Route
+          path={this.props.match.url + "/:id"}
+          exact
+          component={FullPost}
+        />
+      </div>
+    );
+  }
 }
 
 export default Posts;
